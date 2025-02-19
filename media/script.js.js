8a4f988a97f68acd06b4f045d6d8b57084113bbe
Michele1421 <michelevicino54@gmail.com>
2025-02-19 10:16:57 +0100

    Add files via upload

---
+++ media/script.js.js
@@ -0,0 +1,14 @@
+let sommanumeridispari = 0;
+let contanumeridispari = 0;
+
+for (let i = 1; i <= 20; i++) {
+    if (i / 2 === 0) {
+        console.log(i); 
+    } else {
+        sommanumeridispari  i; 
+        contanumerispari++; 
+    }
+}
+
+let media = sommanumeridispari / contanumeriispari;
+console.log("Media dei numeri dispari:", media);
