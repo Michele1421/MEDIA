9a5630fc010472d969099f495c0508eef4913dd3
Michele1421 <michelevicino54@gmail.com>
2025-02-25 15:23:08 +0100

    modifica

--- media/script.js.js
+++ media/script.js.js
@@ -2,13 +2,13 @@ let sommanumeridispari = 0;
 let contanumeridispari = 0;
 
 for (let i = 1; i <= 20; i++) {
-    if (i / 2 === 0) {
+    if (i % 2 == 0) {
         console.log(i); 
     } else {
-        sommanumeridispari  i; 
-        contanumerispari++; 
+        sommanumeridispari =sommanumeridispari + i; 
+        contanumeridispari++; 
     }
 }
 
-let media = sommanumeridispari / contanumeriispari;
+let media = sommanumeridispari / contanumeridispari;
 console.log("Media dei numeri dispari:", media);
